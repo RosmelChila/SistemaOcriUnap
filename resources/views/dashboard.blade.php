5f35f5ebb4feb81ce85a8676e4c78685aadc1271
RosmelChila <88019273+RosmelChila@users.noreply.github.com>
2022-12-03 17:56:26 -0500

    ROSMEL
    CAMBIOS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -113,11 +113,11 @@
                         <tr
                             class=" text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-900">
                             <th class="px-2 py-2">N°</th>
-                            <th class="px-2 py-2 w-1/3">TÍTULO</th>
-                            <th class="px-2 py-2 w-1/5">RESOLUCIÓN</th>
-                            <th class="px-2 py-2 w-1/6">ESTADO</th>
-                            <th class="px-2 py-2 w-1/6">EXPIRACIÓN</th>
-                            <th class="px-2 py-2 w-1/6">ACCIONES</th>
+                            <th class="px-2 py-2 w-1/3 text-center">TÍTULO</th>
+                            <th class="px-2 py-2 w-1/5 text-center">RESOLUCIÓN</th>
+                            <th class="px-2 py-2 w-1/6 text-center">ESTADO</th>
+                            <th class="px-2 py-2 w-1/6 text-center">EXPIRACIÓN</th>
+                            <th class="px-2 py-2 w-1/6 text-center">VER CONVENIO</th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-900">
@@ -141,30 +141,12 @@
                                     Por vencer
                                 </button></div>
                             </td>
-                            <td class="px-3 py-3 text-xs">
+                            <td class="px-3 py-3 text-xs text-center">
                                 27/12/2300
-                            <td class="px-3 py-3">
-                                <div class="flex items-center space-x-4 text-sm">
+                            <td class="px-3 py-3 text-center">
+                                <div class="space-x-4 text-sm">
                                     <button
-                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-sky-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                        aria-label="Edit">
-                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                            <path
-                                                d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                    <button
-                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-sky-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                        aria-label="Delete">
-                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </button>
-                                    <button
-                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-sky-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                        class="px-2 py-2 text-sm font-medium leading-5 text-sky-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                                         aria-label="Delete">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                             fill="currentColor" class="w-5 h-5">
