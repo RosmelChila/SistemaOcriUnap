806fe24f768c604bb07302a1638e7ab4c9193552
RosmelChila <88019273+RosmelChila@users.noreply.github.com>
2022-12-03 18:02:05 -0500

    ros
    ultimo cambio

--- resources/views/administrador/filtro/internacional.blade.php
+++ resources/views/administrador/filtro/internacional.blade.php
@@ -3,17 +3,9 @@
     <div class="col-span-1">
         <aside class="rounded-lg bg-dark-eval-1" aria-label="Sidebar">
             <div class="overflow-y-auto py-4 px-3 bg-gray-50 dark:bg-gray-900 rounded-lg grid justify-center">
+                    {{-- esta lista es para filtro de paises --}}
                <ul class="space-y-2">
                   <li class="">
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-sky-800">
-                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                           <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
-                           <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
-                         </svg>
-                       <span class="ml-3 text-center">ARGENTINA</span>
-                    </a>
-                 </li>
-                 <li class="">
                     <a href="#" class="flex items-center p-2 text-base font-normal text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-sky-800">
                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                            <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
@@ -31,6 +23,7 @@
         <div class="container grid mx-auto ">
             <div class="w-full overflow-hidden rounded-lg shadow-xs">
                 <div class="w-full overflow-x-auto ">
+                        {{-- esta tabla es para el contenido --}}
                     <table class="table-fixed ">
                         <thead>
                             <tr
