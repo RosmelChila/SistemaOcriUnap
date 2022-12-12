d2243cbf9146c57d27f7b06efd21e43786f6f90e
RosmelChila <88019273+RosmelChila@users.noreply.github.com>
2022-12-11 19:16:45 -0500

    cabio

--- resources/views/livewire/agregar-component.blade.php
+++ resources/views/livewire/agregar-component.blade.php
@@ -1,82 +1,96 @@
 <div>
+
     <form >
         @csrf
         <div class="grid grid-cols-1 gap-3">
             {{-- para datos 1 --}}
-            <div class="grid grid-rows-2 grid-flow-col dark:bg-gray-900 rounded-lg py-4">
+            <div class="shadow-lg grid grid-rows-2 grid-flow-col dark:bg-zinc-900 rounded-lg py-4">
                 <div class="px-4 grid content-center col-span-1">
                     <label class="label">
-                        <span class="label-text text-gray-900 dark:text-gray-100">Resolución</span>
+                        <span class="label-text text-zinc-900 dark:text-zinc-100">Resolución</span>
                     </label>
                     <input wire:model="resolution" type="text" name="resolution" placeholder="RESOLUCION 123-XX"
-                        class="input input-bordered input-success input-sm  w-full input-md bg-white dark:bg-gray-800 dark:input dark:input-sm" />
-                        @error('resolution') <span class="text-danger">{{ $message }}</span>@enderror
+                        class="input input-bordered input-success input-sm  w-full input-md bg-white dark:bg-zinc-800 dark:input dark:input-sm" />
+                    <x-input-error for="resolution" />
+
                 </div>
                 <div class="px-4  grid content-center row-span-1 col-span-1">
                     <label class="label">
-                        <span class="label-text text-gray-900 dark:text-gray-100">Título</span>
+                        <span class="label-text text-zinc-900 dark:text-zinc-100">Título</span>
                     </label>
                     <input wire:model='title' type="text" name="title" placeholder="CONVENIO ......"
-                        class="input input-bordered input-success input-sm  w-full input-md bg-white dark:bg-gray-800 dark:input dark:input-sm" />
+                        class="input input-bordered input-success input-sm  w-full input-md bg-white dark:bg-zinc-800 dark:input dark:input-sm" />
+                    <x-input-error for="title" />
+
                 </div>
                 <div class=" px-4 row-span-2">
                     <label class="label">
-                        <span class="label-text text-gray-900 dark:text-gray-100">Objetivos</span>
+                        <span class="label-text text-zinc-900 dark:text-zinc-100">Objetivos</span>
                     </label>
-                    <textarea wire:model='objetive' name="objetive" class="textarea textarea-success w-full bg-white dark:bg-gray-900 dark:textarea h-24"
+                    <textarea wire:model='objetive' name="objetive" class="textarea textarea-success w-full bg-white dark:bg-zinc-900 dark:textarea h-24"
                         placeholder="OBJETIVOS"></textarea>
+                    <x-input-error for="objetive" />
+
                 </div>
             </div>
 
-            {{-- para los dotos 2 --}}
-            <div>
-                <div class="grid grid-row md:grid-cols-4 dark:bg-gray-900 rounded-lg py-4">
+            <div class="shadow-lg  dark:bg-zinc-900 rounded-lg py-4">
+                <div class="grid grid-row md:grid-cols-4 ">
                     <div class="px-4 grid content-center col-span-2">
                         <label class="label">
-                            <span class="label-text text-gray-900 dark:text-gray-100">Fecha de
+                            <span class="label-text text-zinc-900 dark:text-zinc-100">Fecha de
                                 suscripción</span>
                         </label>
                         <input wire:model='subscription' name="subscription" type="date" placeholder=""
-                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-gray-800 w-full" />
+                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-zinc-800 w-full" />
+                    <x-input-error for="subscription" />
+
                     </div>
                     {{-- años vencimiento --}}
                     <div class="px-4 grid content-center">
                         <label class="label">
-                            <span class="label-text text-gray-900 dark:text-gray-100">Vencimiento en años</span>
+                            <span class="label-text text-zinc-900 dark:text-zinc-100">Vencimiento en años</span>
                         </label>
                         <input wire:model='years' type="number" name="years" value="0"
-                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-gray-800 w-full" />
+                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-zinc-800 w-full" />
+                    <x-input-error for="years" />
+
                     </div>
                     <div class="px-4 grid content-center">
                         <label class="label">
-                            <span class="label-text text-gray-900 dark:text-gray-100">Vencimiento en mes</span>
+                            <span class="label-text text-zinc-900 dark:text-zinc-100">Vencimiento en mes</span>
                         </label>
                         <input type="number" wire:model='months' name="months" value="0"
-                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-gray-800 w-full" />
+                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-zinc-800 w-full" />
                     </div>
 
                     <div class="px-4 grid content-center col-span-2">
                         <label class="label">
-                            <span class="label-text text-gray-900 dark:text-gray-100">Fecha aprobación
+                            <span class="label-text text-zinc-900 dark:text-zinc-100">Fecha aprobación
                                 C.U.</span>
                         </label>
                         <input wire:model='approbation' name="approbation" type="date" placeholder=""
-                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-gray-800 w-full" />
+                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-zinc-800 w-full" />
+                    <x-input-error for="approbation" />
+
                     </div>
 
                     <div class="px-4 grid content-center">
                         <label class="label">
-                            <span class="label-text text-gray-900 dark:text-gray-100">Vencimiento en dias</span>
+                            <span class="label-text text-zinc-900 dark:text-zinc-100">Vencimiento en dias</span>
                         </label>
                         <input wire:model='days' type="number" name="days" value="0"
-                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-gray-800 w-full" />
+                            class="input input-bordered input-success input-sm bg-white dark:input dark:input-sm dark:bg-zinc-800 w-full" />
                     </div>
                 </div>
-                <div>
-                    <div class="grid grid-row md:grid-cols-4 dark:bg-gray-900 rounded-lg py-4">
+            </div>
+
+            <div class="shadow-lg dark:bg-zinc-900 rounded-lg py-4">
+                <div class="mt-4">
+                    <div class="grid grid-row md:grid-cols-4 dark:bg-zinc-900 rounded-lg mt-4">
                         <div class="px-4 grid content-center col-span-2">
                             <label class="label">
-                                <span class="label-text text-gray-900 dark:text-gray-100">Cobertura</span>
+                                <span class="label-text text-zinc-900 dark:text-zinc-100">Cobertura</span>
                             </label>
                             <select wire:model='coverage_id' name="coverage_id" value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -86,10 +100,12 @@
                                         {{ $name }}</option>
                                 @endforeach
                             </select>
+                    <x-input-error for="coverage_id" />
+
                         </div>
                         <div class="px-4 grid content-center col-span-2">
                             <label class="label">
-                                <span class="label-text text-gray-900 dark:text-gray-100">Convenio con:</span>
+                                <span class="label-text text-zinc-900 dark:text-zinc-100">Convenio con:</span>
                             </label>
                             <select wire:model='organization_id' name="organization_id" value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -99,6 +115,8 @@
                                         {{ $name }}</option>
                                 @endforeach
                             </select>
+                    <x-input-error for="organization_id" />
+
                         </div>
 
 
@@ -110,7 +128,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Pais</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Pais</span>
                                 </label></button>
                             <select wire:model='countryid' value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -122,6 +140,8 @@
                                     <option value="">No hay paisess</option>
                                 @endforelse
                             </select>
+                    <x-input-error for="countryid" />
+
                         </div>
                         <div class="px-4 grid content-center">
                             <button class="flex row items-center">
@@ -130,7 +150,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Región</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Región</span>
                                 </label></button>
                             <select wire:model='regionid' value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -150,7 +170,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">provincia</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">provincia</span>
                                 </label></button>
                             <select wire:model='provinceid' value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -170,7 +190,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Distrito</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Distrito</span>
                                 </label></button>
                             <select wire:model="districtid" value="{{ old('id') }}"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -190,7 +210,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Localización</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Localización</span>
                                 </label></button>
                             <select wire:model="location" name="location"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -199,6 +219,8 @@
                                 <option>NACIONAL</option>
                                 <option>LOCAL</option>
                             </select>
+                        <x-input-error for="location" />
+
                         </div>
                         <div class="px-4 grid content-center">
                             <button class="flex row items-center">
@@ -207,7 +229,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Sector</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Sector</span>
                                 </label></button>
                             <select wire:model="sector" name="sector"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -215,6 +237,8 @@
                                 <option>PUBLICO</option>
                                 <option>PRIVADO</option>
                             </select>
+                    <x-input-error for="sector" />
+
                         </div>
                         <div class="px-4 grid content-center">
                             <button class="flex row items-center">
@@ -223,7 +247,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg> <label class="label">
-                                    <span class="label-text text-gray-900 dark:text-gray-100">Organización</span>
+                                    <span class="label-text text-zinc-900 dark:text-zinc-100">Organización</span>
                                 </label></button>
                             <select wire:model="organization" name="organization"
                                 class="select select-success select-sm bg-white dark:select dark:select-sm">
@@ -231,18 +255,21 @@
                                 <option>UNIVERSIDAD</option>
                                 <option>EMPRESA</option>
                             </select>
+                    <x-input-error for="organization" />
+
                         </div>
                         <div class="px-4 grid content-center">
                         </div>
                     </div>
                 </div>
+            </div>
 
-                {{-- para los responsables --}}
-                <div class="p-4 dark:bg-gray-900 rounded-lg py-4">
+            <div class="shadow-lg dark:bg-zinc-900 rounded-lg py-4">
+                <div class="p-4 dark:bg-zinc-900 rounded-lg py-4">
                     <div x-data="{ show: true }" class="w-full ">
                         <label for="check-menu" class="flex flex-row  align-middle cursor-pointer w-70px">
                             <input type="checkbox" id="check-menu" class="hidden" x-model="show" />
-                            <span class="duration-[400ms] label-text text-gray-900 dark:text-gray-200">Seleccionar
+                            <span class="duration-[400ms] label-text text-zinc-900 dark:text-zinc-200">Seleccionar
                                 Responsables</span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="1.5" stroke="#1FB2A6" class="w-5 h-5">
@@ -251,7 +278,7 @@
                             </svg>
                         </label>
                         <div x-show="show"
-                            class=" px-4 space-y-6 overflow-hidden rounded-lg shadow-md dark:bg-gray-900 dark:text-gray-200 grid justify-items-start">
+                            class=" px-4 space-y-6 overflow-hidden rounded-lg shadow-md dark:bg-zinc-900 dark:text-zinc-200 grid justify-items-start">
                             <div class="form-control px-2 grid grid-cols-10">
                                 @if (sizeof($responsibles) > 0)
                                     @foreach ($responsibles as $id => $name)
@@ -263,17 +290,22 @@
                                         </label>
                                     @endforeach
                                 @endif
+                    <x-input-error for="responsible" />
+
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                {{-- para cargar el pdf --}}
-                <div class="grid content-center p-4 dark:bg-gray-900 rounded-lg py-4">
-                    <span class="label-text text-gray-900 dark:text-gray-100 py-2 ">Convenio Escaneado (PDF)</span>
+            <div class="shadow-lg dark:bg-zinc-900 rounded-lg py-4">
+                <div class="grid content-center p-4 dark:bg-zinc-900 rounded-lg py-4">
+                    <span class="label-text text-zinc-900 dark:text-zinc-100 py-2 ">Convenio Escaneado (PDF)</span>
                     <input wire:model='paths' name="paths" id="paths" type="file"
-                        class="file-input file-input-xs file-input-bordered file-input-accent w-full max-w-xs bg-gray-200 dark:bg-gray-900" />
+                        class="file-input file-input-xs file-input-bordered file-input-accent w-full max-w-xs bg-zinc-200 dark:bg-zinc-900" />
                 </div>
+                <x-input-error for="paths" />
+
             </div>
 
             <div class="">
